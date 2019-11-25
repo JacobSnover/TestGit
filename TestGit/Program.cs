9ae26c832ec06b8cc73bfdf310e5df8d8188487d
Jason Gardner <jasonagardner@gmail.com>
2019-11-25 18:55:27 -0500

    Second commit.

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -7,6 +7,7 @@ namespace TestGit
         static void Main(string[] args)
         {
             //jacob was here
+            //we made it
             Console.WriteLine("Hello World!");
         }
     }
